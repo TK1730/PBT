98b08b718a18a1ccfdde0377d24030c66d671fad
TK1730 <kazuki.iyh@gmail.com>
2025-01-27 17:15:26 +0900

    repeatクラスの実装

---
+++ utils/repeat.py
@@ -0,0 +1,30 @@
+class Repeat(object):
+    def __init__(self, hour):
+        self.rep_limit = hour * 2
+        self.rep_now = 0
+        self.flag_finish = False
+
+    def repeat_count(self):
+        """
+            繰り返し回数をカウント
+        """
+        self.rep_now += 1
+
+    def completion_judg(self):
+        """
+            繰り返し回数に達したか判定
+        """
+        if self.rep_limit == self.rep_now:
+            self.flag_finish = True
+
+
+if __name__ == '__main__':
+    repe = Repeat(4)
+    print(repe.flag_finish)
+    for i in range(10):
+        print(i)
+        repe.repeat_count()
+        repe.completion_judg()
+        if repe.flag_finish:
+            break
+print("finish")
