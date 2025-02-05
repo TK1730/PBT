44bb4cd0160b15d961783626d70f88303e0c168b
TK1730 <kazuki.iyh@gmail.com>
2025-02-05 22:17:30 +0900

    ポモドーロタイマー完成

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-from Timer import timer
+from utils import GUI, pomodotimer
+
+def main():
+    pomo = pomodotimer.PomodoroTimer()
+    app = GUI.PomodoroWindow(pomo)
+    app.mainloop()
+
+if __name__ == '__main__':
+    main()
--- utils/GUI.py
+++ utils/GUI.py
@@ -5,45 +5,56 @@ from tkinter import messagebox
 
 from pkg_resources import WorkingSet
 
-import utils.timer as timer
+from utils.pomodotimer import PomodoroTimer
 
 class PomodoroWindow(tk.Tk):
     """
     Window表示用
     """
-    def __init__(self):
+    def __init__(self, pomodoro_timer):
         super().__init__()
 
         self.title("Pomodoro Blackout Timer")
         self.geometry("500x500")
 
-        # すべてのフレームを辞書に保存
-        self.frames = {}
+        self.pomodoro_timer = pomodoro_timer
+        self.current_window = None  # 現在のウィンドウを保持
 
-        # フレーム（ウィンドウ）を作成
-        self.frames = {}
+        self.show_start_window()
 
-        # 作業用タイマー
-        self.work_timer = timer.TimeWork(25)
-        # 休憩用タイマー
-        self.frames = timer.TimeRest(5)
-
-        # フレーム（ウィンドウ）を作成
-        for F in (StartWindow, WorkingWindow, RestWindow, FinishWindow):
-            frame = F(self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(StartWindow)
+    def show_start_window(self):
+        """
+        スタートウィンドウ表示
+        """
+        self.change_window(StartWindow)
     
-    def show_frame(self, page: tk.Frame):
+    def show_working_window(self):
         """
-            指定したページを表示する
-        Args:
-            page ( window ): windowのクラス
+        作業ウィンドウ表示
         """
-        frame = self.frames[page]
-        frame.tkraise()
+        self.change_window(WorkingWindow)
+
+    def show_rest_window(self):
+        """
+        休憩ウィンドウ表示
+        """
+        self.change_window(RestWindow)
+
+    def show_finish_window(self):
+        """
+        終了ウィンドウ表示
+        """
+        self.change_window(FinishWindow)
+
+    def change_window(self, window):
+        """
+        ウィンドウ変更
+        """
+        if self.current_window is not None:
+            self.current_window.destroy()
+
+        self.current_window = window(self, self.pomodoro_timer)
+        self.current_window.pack()
 
 
 
@@ -51,11 +62,12 @@ class StartWindow(tk.Frame):
     """
     スタートウィンドウ
     """
-    def __init__(self, master):
+    def __init__(self, master, pomodoro_timer: PomodoroTimer):
         """
         スタートウィンドウに使用する変数
         """
         super().__init__(master)
+        self.pomodoro_timer = pomodoro_timer
 
         self.title = "ポモドーロタイマー"
         self.subtitle = "限界を超えろ!"
@@ -79,98 +91,112 @@ class StartWindow(tk.Frame):
     
     def StartTimer(self):
         # Timerクラスに総作業時間を代入
-        timer.Timer.set_total_time(self.user_input_box.get())
-        self.master.show_frame(WorkingWindow)
+        self.pomodoro_timer.input_repeat(int(self.user_input_box.get()))
+        self.master.show_working_window()
 
 
 class WorkingWindow(tk.Frame):
-    def __init__(self, master):
+    def __init__(self, master, pomodoro_timer: PomodoroTimer):
         """
             作業ウィンドウ
         """
         super().__init__(master)
+        self.pomodoro_timer = pomodoro_timer
 
         self.message = "自分を超えろ！"
         message_label = tk.Label(self, text=self.message, font=("Arial", 9))
         message_label.pack(pady=5)
 
-        self.left_time_label = tk.Label(self, text="", font=("Arial", 20))
+        # 作業時間を表示
+        self.minutes, self.sec = self.pomodoro_timer.time_work.Transform()
+        self.left_time_label = tk.Label(self, text=f"{self.minutes:2}:{self.sec:2}", font=("Arial", 20))
         self.left_time_label.pack(pady=10)
 
-        self.left_lim_label = tk.Label(self, text=f"{now_lim} / {left_lim}", font=("Arial", 9))
+        self.left_lim_label = tk.Label(self, text=f"{self.pomodoro_timer.rep.rep_now} / {self.pomodoro_timer.rep.rep_limit}", font=("Arial", 9))
         self.left_lim_label.pack(pady=5)
 
-    def update_timer(self):
-        pass
+        # 作業時間の更新
+        self.update_timer()
 
-class RestWindow(PomodoroWindow):
-    """
-    休憩中ウィンドウ
-    """
-    def __init__(self):
+    def update_timer(self):
+        """
+            タイマー更新
+        """
+        self.pomodoro_timer.time_work.CountDown()
+        self.minutes, self.sec = self.pomodoro_timer.time_work.Transform()
+        self.left_time_label.config(text=f"{self.minutes:2}:{self.sec:2}")
+        self.left_lim_label.config(text=f"{self.pomodoro_timer.rep.rep_now} / {self.pomodoro_timer.rep.rep_limit}")
+
+        if self.pomodoro_timer.time_work.currnet_time == 0:
+            self.master.show_rest_window()
+        else:
+            self.after(1000, self.update_timer)
+
+class RestWindow(tk.Frame):
+    def __init__(self, master, pomodoro_timer: PomodoroTimer):
         """
         restウィンドウで使用する変数
         """
-        super().__init__()
+        super().__init__(master)
+        self.pomodoro_timer = pomodoro_timer
 
         self.rest_methods = ["つむつむ","仮眠","休憩"]
-
-        self.left_time_label = None
-        self.message_label = None
-
-    def rest_window(self, minutes, sec):
-        """
-        休憩中ウィンドウ表示
-        """
         self.rest_way()
 
-        self.message_label = tk.Label(self.root, text=f"休憩方法　:　{self.chosen_method}", font=("Arial", 9))
+        # 休憩時のメッセージ表示
+        self.message_label = tk.Label(self, text=f"休憩方法 : {self.chosen_method}", font=("Arial", 9))
         self.message_label.pack(pady=5)
 
-        self.left_time_label = tk.Label(self.root, text=f"{minutes} : {sec}", font=("Arial", 20))
+        # 休憩時間を表示
+        self.minutes, self.sec = self.pomodoro_timer.time_rest.Transform()
+        self.left_time_label = tk.Label(self, text=f"{self.minutes:2} : {self.sec:2}", font=("Arial", 20))
         self.left_time_label.pack(pady=5)
 
-        self.root.mainloop()
+        # 休憩時間の更新
+        self.update_timer()
 
     def rest_way(self):
         """
         休憩方法を獲得
         """
         self.chosen_method = random.choice(self.rest_methods)
+    
+    def update_timer(self):
+        """
+        タイマー更新
+        """
+        self.pomodoro_timer.time_rest.CountDown()
+        self.minutes, self.sec = self.pomodoro_timer.time_rest.Transform()
+        self.left_time_label.config(text=f"{self.minutes:2}:{self.sec:2}")
 
-class FinishWindow(PomodoroWindow):
-    """
-    終了ウィンドウ
-    """
-    def __init__(self):
+        if self.pomodoro_timer.time_rest.currnet_time == 0:
+            if self.pomodoro_timer.rep.flag_finish:
+                self.master.show_finish_window()
+            else:
+                self.master.show_working_window()
+        else:
+            self.after(1000, self.update_timer)
+
+class FinishWindow(tk.Frame):
+    def __init__(self, master, pomodoro_timer: PomodoroTimer):
         """
         終了ウィンドウで使用する変数
         """
-        super().__init__()
+        super().__init__(master)
+        self.pomo_timer = pomodoro_timer
 
         self.title = "お疲れさまでした"
         self.subtitle = "水分補給してゆっくり休みましょう"
 
         self.title_label = None
         self.subtitle_label = None
-
-    def finish_window(self):
-        """
-        終了ウィンドウ表示
-        """
         self.title_label = tk.Label(self.root, text=self.title, font=("Arial", 30))
         self.title_label.pack(pady=5)
 
         self.subtitle_label = tk.Label(self.root, text=self.subtitle, font=("Arial",10))
         self.subtitle_label.pack(pady=5)
 
-        self.root.mainloop()
-
 if __name__ == "__main__":
-    window = StartWindow()
-    window.start_window()
-    work = timer.TimeWork(25)
-    print(work.total_time)
-    print("Finish")
+    pass
 
 
